5b2bac171df4e46db634edc787cab85b4fd4f19d
xx <yingxiaowoxx@163.com>
2023-03-24 16:38:54 +0800

    add picgo keymap

--- lua/xx/core/keymaps.lua
+++ lua/xx/core/keymaps.lua
@@ -21,12 +21,10 @@ keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
 keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
 
-
 keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
 keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
-
 
 ----------------------
 -- Plugin Keybinds
@@ -59,3 +57,6 @@ keymap.set("n", "<leader>hc", ":HopChar2<cr>")
 keymap.set("n", "<leader>hcc", ":HopChar2MW<cr>")
 keymap.set("n", "<leader>hl", ":HopLine<cr>")
 keymap.set("n", "<leader>hls", ":HopLineStart<cr>")
+
+-- Picgo
+keymap.set("n", "<leader>pg", ":UploadClipboard<cr>")
